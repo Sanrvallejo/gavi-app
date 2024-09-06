8e2f2b7b9938ee863dd7c2e677697d517fd3a2d2
sanrvallejo <zanti-23@hotmail.com>
2024-09-05 22:27:20 -0500

    creación proyecto

---
+++ gavi/gavi/src/main/java/com/sena/gavi/GaviApplication.java
@@ -0,0 +1,13 @@
+package com.sena.gavi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaviApplication.class, args);
+	}
+
+}
---
+++ gavi/gavi/src/test/java/com/sena/gavi/GaviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena.gavi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
