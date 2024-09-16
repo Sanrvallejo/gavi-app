e28184d2022b8205e6edba7cfaf19f258506df59
sanrvallejo <zanti-23@hotmail.com>
2024-09-15 23:05:11 -0500

    metodo create para traer la vista create

--- gavi/gavi/src/main/java/com/sena/gavi/controllers/ProductController.java
+++ gavi/gavi/src/main/java/com/sena/gavi/controllers/ProductController.java
@@ -1,6 +1,8 @@
 package com.sena.gavi.controllers;
 
-import com.sena.gavi.model.repository.ProductRepository;
+import com.sena.gavi.enums.Category;
+import com.sena.gavi.enums.Tax;
+import com.sena.gavi.enums.Units;
 import com.sena.gavi.services.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -22,7 +24,12 @@ public class ProductController {
     }
 
     @GetMapping("/create-product")
-    public String createProduct() {
+    public String createProduct(Model model) {
+        //pasar los enum como atributos para los select de la vista
+        model.addAttribute("categories", Category.values());//categorias
+        model.addAttribute("units", Units.values());//unidades
+        // proveedores
+        model.addAttribute("taxes", Tax.values());//impuestos
         return "admin/create-product";
     }
 }
