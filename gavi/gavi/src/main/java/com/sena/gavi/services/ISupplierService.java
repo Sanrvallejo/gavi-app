f53bc0b32e8e442f11dc4af01148cba91d4eb3a9
sanrvallejo <zanti-23@hotmail.com>
2024-10-04 19:20:32 -0500

    interfaz de servicios para proveedor (supplier)

---
+++ gavi/gavi/src/main/java/com/sena/gavi/services/ISupplierService.java
@@ -0,0 +1,15 @@
+package com.sena.gavi.services;
+
+import com.sena.gavi.model.entities.Supplier;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ISupplierService {
+    Optional<Supplier> findById(UUID id);
+    void create (Supplier supplier);
+    List<Supplier> findAll();
+    void update (Supplier supplier);
+
+}
