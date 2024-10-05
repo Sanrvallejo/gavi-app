586ca300b14a236540ceb0d285f7e3ec77c5dfa3
sanrvallejo <zanti-23@hotmail.com>
2024-10-04 21:08:51 -0500

    controller supplier con vista para listar todos

---
+++ gavi/gavi/src/main/java/com/sena/gavi/controllers/SupplierController.java
@@ -0,0 +1,20 @@
+package com.sena.gavi.controllers;
+
+import com.sena.gavi.services.ISupplierService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/suppliers")
+public class SupplierController {
+
+    @Autowired
+    private ISupplierService supplierService;
+
+    @GetMapping("/show")
+    public String showSuppliers() {
+        return "admin/show-suppliers";
+    }
+}
