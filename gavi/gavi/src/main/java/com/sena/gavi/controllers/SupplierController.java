98e2b5c5a394c34e4ac1cb0714703d9245f0c4ed
sanrvallejo <zanti-23@hotmail.com>
2024-10-06 00:02:51 -0500

    correccion: ruta editar proveedor (/edit-supplier/{id})

--- gavi/gavi/src/main/java/com/sena/gavi/controllers/SupplierController.java
+++ gavi/gavi/src/main/java/com/sena/gavi/controllers/SupplierController.java
@@ -5,10 +5,7 @@ import com.sena.gavi.services.ISupplierService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
@@ -53,17 +50,17 @@ public class SupplierController {
         return "redirect:/suppliers";
     }
 
-    @GetMapping("/edit-supplier")
+    @GetMapping("/edit-supplier/{id}")
     public String editSupplier(
-            @ModelAttribute("supplierId") UUID supplierId,
+            @PathVariable UUID id,
             Model model) {
         model.addAttribute("nameHeader", viewName);
-        Optional<Supplier> foundSupplier = supplierService.findById(supplierId);
+        Optional<Supplier> foundSupplier = supplierService.findById(id);
         Supplier supplier = foundSupplier.get();
 
         model.addAttribute("supplier", supplier);
 
-        return "redirect:admin/edit-supplier";
+        return "admin/edit-supplier";
     }
 
 
