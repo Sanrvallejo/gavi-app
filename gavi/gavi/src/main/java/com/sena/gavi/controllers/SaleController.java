da82585dc2df41ee1016f18fcdbd09a8adf5bca4
sanrvallejo <zanti-23@hotmail.com>
2024-10-26 22:08:30 -0500

    ruta/metodo: crear venta

--- gavi/gavi/src/main/java/com/sena/gavi/controllers/SaleController.java
+++ gavi/gavi/src/main/java/com/sena/gavi/controllers/SaleController.java
@@ -1,5 +1,6 @@
 package com.sena.gavi.controllers;
 
+import com.sena.gavi.enums.PaymentMethod;
 import com.sena.gavi.model.entities.Sale;
 import com.sena.gavi.services.ISalesService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +14,23 @@ import java.util.List;
 @Controller
 @RequestMapping("/sales")
 public class SaleController {
+
+    String viewName = "Sales";
+
     @Autowired
     private ISalesService saleService;
 
-    @GetMapping("/sales")
+    @GetMapping("")
     public String showSales(Model model) {
+        model.addAttribute("nameHeader", viewName);
         model.addAttribute("salesList", saleService.findAll());
         return "intern/show-sales";
+    }
+
+    @GetMapping("/create-sale")
+    public String createSale(Model model) {
+        model.addAttribute("nameHeader", viewName);
+        model.addAttribute("paymentMethods", PaymentMethod.values());
+        return "intern/create-sale";
     }
 }
